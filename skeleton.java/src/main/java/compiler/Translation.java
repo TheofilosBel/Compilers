f55e304d697b7b7f6ba1c49c2740fa47737cbcd7
TheofilosBel <theofilasp@gmail.com>
2017-03-30 16:40:01 +0300

    Print of vars done

--- skeleton.java/src/main/java/compiler/Translation.java
+++ skeleton.java/src/main/java/compiler/Translation.java
@@ -2,34 +2,78 @@ package compiler;
 
 import compiler.analysis.*;
 import compiler.node.*;
+import java.util.Collections;
+
 
 public class Translation extends DepthFirstAdapter{
-	/*
-	public void outADefVariableDefinition(ADefVariableDefinition node) { 
-		System.out.print(node);
-	}
     
-	public void caseTId(TId node)
-	{
-		System.out.println(node);
-	}*/
+    int indentation = 0;
+    
+    private void addIndentationLevel() {
+        indentation++;
+    }
+
+    private void removeIndentationLevel() {
+        indentation--;
+    }
+
+    private void printIndentation() {
+        System.out.print(String.join("", Collections.nCopies(indentation, " ")));
+    }
+    
+    @Override
+    public void inAType(AType node)
+    {
+        /* See if its an array or not */
+        System.out.print(" type =");
+        System.out.print(node.getDataType().toString());
+        
+        /*If it's an array print it */
+    }
+    
+    @Override
+    public void inAExistingArrayDec(AExistingArrayDec node)
+    {
+        System.out.print(" with dimensions =");
+        System.out.print(node.getIntConst().size());
+        System.out.print(" and vals =");
+        System.out.println(node.getIntConst());
+        
+    }
+    
+    @Override
+    public void inANotExistingArrayDec(ANotExistingArrayDec node)
+    {
+        System.out.println();
+    }
+   
 	
 	@Override
-	public void outAVar(AVar node)
+	public void inAVar(AVar node)
     {
+	    
+	    /* Print the right indentation */
+	    printIndentation();
+	    
 	    /*Print the ids */
 	    System.out.print("VARS list=");
         System.out.print(node.getIdList());
-        System.out.print(" type = ");
-        /* Print the type */
-        System.out.println(node.getType().toString());
     }
 	
 	@Override
-	public void outAVarList(AVarList node)
+	public void inAVarList(AVarList node)
     {
         System.out.print("Var def's found : ");
         System.out.println(node.getVList().size());
+        addIndentationLevel();
     }
+	
+	
+	@Override
+    public void outAVarList(AVarList node)
+    {
+	    removeIndentationLevel();
+    }
+	
 	
 }
