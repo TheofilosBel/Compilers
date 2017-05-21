9aa6d38e707eed02f7c2982172841d306ca351a8
TheofilosBel <theofilasp@gmail.com>
2017-05-21 12:57:52 +0300

    merger

--- skeleton.java/src/main/java/compiler/semanticAnalysis/FuncDefInfo.java
+++ skeleton.java/src/main/java/compiler/semanticAnalysis/FuncDefInfo.java
@@ -3,6 +3,9 @@ package compiler.semanticAnalysis;
 import compiler.node.PDataType;
 import compiler.node.PFparList;
 
+/*
+ * Class to keep the info of a function definition
+ */
 public class FuncDefInfo extends FunctionInfo {
 
     public FuncDefInfo(PDataType rettype, PFparList argList, String name) {
