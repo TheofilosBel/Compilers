ccdbad61cbbade2e778d422add9ae4e5826ae766
TheofilosBel <theofilasp@gmail.com>
2017-05-23 11:38:06 +0300

    Inserted CurrenFunction

---
+++ skeleton.java/src/main/java/compiler/intermediateCode/Quads.java
@@ -0,0 +1,26 @@
+package compiler.intermediateCode;
+
+public class Quads {
+
+    /* Static integer to give the right label to the quads */
+    private static int labelCount = 0;
+    
+    private String opName;
+    private String x;
+    private String y;
+    private String z;
+
+    
+    /* Returns the number of the next quad */
+    public static int nextQuad() {
+        return labelCount + 1;
+    }
+    
+    /* Its like fuction GenQuad*/
+    public Quads(String opName, String x, String y, String z) {
+        this.opName = opName;
+        this.x      = x;
+        this.y      = y;
+        this.z      = z;
+    }
+}
